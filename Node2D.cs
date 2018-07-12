4801f0a9a9b5585c651d1c985106a83664813d7d
Nathan <dynamonath@nathanjwtx.me>
2018-07-12 16:37:48 -0500

    Added JSON file to hold stories and inputs
    Updated code to enable random selection of story and corresponding inputs from a JSON file.

--- Node2D.cs
+++ Node2D.cs
@@ -2,6 +2,17 @@ using Godot;
 using static Godot.GD;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using File = Godot.File;
+
+public class Story
+{
+    public List<string> inputs = new List<string>();
+    public string story;
+}
 
 public class Node2D : Godot.Node2D
 {
@@ -9,14 +20,8 @@ public class Node2D : Godot.Node2D
     public RichTextLabel StoryText;
     public LineEdit TextEnter;
     public List<string> PlayerWords = new List<string>();
-    public readonly List<string> prompts = new List<string>
-    {
-        "a name",
-        "a thing",
-        "a feeling",
-        "another feeling",
-        "some things"
-    };
+
+    public List<string> Prompts = new List<string>();
 
     private string _story;
     
@@ -25,11 +30,12 @@ public class Node2D : Godot.Node2D
         ButtonLabel = (RichTextLabel) GetNode("Background/EnterButton/ButtonLabel");
         TextEnter = (LineEdit) GetNode("Background/TextBox");
         StoryText = (RichTextLabel) GetNode("Background/StoryText");
+        GetFromJSON("loopy_lips.JSON");
         ResetTextbox();
         ButtonLabel.Text = "Ok";
         StoryText.BbcodeEnabled = true;
         StoryText.BbcodeText = "Welcome to Loopy Lips. \nPlease follow the prompts for a word or phrase."
-                             + "\nCan I have " + prompts[PlayerWords.Count] + " , please?";
+                             + "\nCan I have " + Prompts[PlayerWords.Count] + " , please?";
     }
 
     private void _on_TextBox_text_entered(String newText)
@@ -53,7 +59,23 @@ public class Node2D : Godot.Node2D
     }
 
     #region HelperMethods
-
+    
+    public void GetFromJSON(string filename)
+    {
+        var file = new File();
+        file.Open(filename, 1);
+        var text = file.GetAsText();
+        List<Story> data = JsonConvert.DeserializeObject<List<Story>>(
+            text);
+//        replace line below with Godot's own method for getting the JSON text from file
+//        System.IO.File.ReadAllText(@"C:\Users\Nathan - User\OneDrive\Godot\Loopy Lips - C#\loopy_lips.json")
+        Random rnd = new Random();
+        var i = rnd.Next(0, data.Count);
+        _story = data[i].story;
+        Prompts = data[i].inputs;
+        file.Close();
+    }
+    
     public void ResetTextbox()
     {
         TextEnter.Text = "";
@@ -61,12 +83,12 @@ public class Node2D : Godot.Node2D
     
     private void PlayerPrompt()
     {
-        StoryText.BbcodeText = "Can I have " + prompts[PlayerWords.Count] + " , please?";
+        StoryText.BbcodeText = "Can I have " + Prompts[PlayerWords.Count] + " , please?";
     }
 
     private bool IsStoryDone()
     {
-        return PlayerWords.Count == prompts.Count;
+        return PlayerWords.Count == Prompts.Count;
     }
 
     private void CheckNumberOfWords()
@@ -83,8 +105,10 @@ public class Node2D : Godot.Node2D
 
     private void TellStory()
     {
-        _story = $"Once upon a time there was {PlayerWords[0]} and he ate a {PlayerWords[1]} and felt" +
-                $" very {PlayerWords[2]}. It was a {PlayerWords[3]} day for all good {PlayerWords[4]}";
+        for (int i = 0; i < PlayerWords.Count; i++)
+        {
+            _story = _story.Replace("%s" + System.Convert.ToString(i), PlayerWords[i]);
+        }
         StoryText.BbcodeText = _story;
         ButtonLabel.Text = "Doogan!";
         EndGame();
@@ -98,9 +122,3 @@ public class Node2D : Godot.Node2D
     #endregion
 
 }
-
-
-
-
-
-
